945eeb869bca19a9cc2ca3e416f2f7578ef7784c
hello <elisa8100@gmail.com>
2026-02-10 08:55:01 +0900

    Update main.js

--- mole/js/main.js
+++ mole/js/main.js
@@ -1,11 +1,5 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 import { startGame } from "./game.js";
 import { loadLeaderboard } from "./leaderboard.js";
-
-export const supabase = createClient(
-  "https://oykepnxnbyltcvcqnfbe.supabase.co",
-  "sb_publishable_85W1hHMVmhILGBbrX-l4wg_qmLCn987"
-);
 
 startGame();
 loadLeaderboard();
