c2c861a96e581ef65d133b69046cbf6670b565dc
hello <elisa8100@gmail.com>
2026-03-11 16:43:13 +0900

    Update admin.js

--- donghae/admin/admin.js
+++ donghae/admin/admin.js
@@ -1,22 +1,32 @@
-const supabaseClient = supabase.createClient(
+const supabase = window.supabase.createClient(
 "https://hkswzghtmeeftjmnnsmx.supabase.co",
 "sb_publishable_5YfaypCKMP8y_H_u3_dBGw_MZ9CIPG9"
-);
+)
+
+function login(){
+
+const pw=document.getElementById("pw").value
+
+if(pw==="donghae2026"){
+
+document.getElementById("login").style.display="none"
+document.getElementById("panel").style.display="block"
+
+}
+
+}
 
 async function addNotice(){
 
-let title = document.getElementById("title").value;
-let content = document.getElementById("content").value;
+const title=document.getElementById("title").value
+const content=document.getElementById("content").value
+const type=document.getElementById("type").value
+const due_date=document.getElementById("date").value
 
-await supabaseClient
+await supabase
 .from("notices")
-.insert([
-{
-title:title,
-content:content
-}
-]);
+.insert([{title,content,type,due_date}])
 
-alert("공지 추가 완료");
+alert("추가 완료")
 
 }
