b69bf4986aaa4b5efd8679c5b9f222de093d8549
hello <elisa8100@gmail.com>
2026-03-11 16:55:22 +0900

    Update admin.js

--- donghae/admin/admin.js
+++ donghae/admin/admin.js
@@ -1,4 +1,4 @@
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
 "https://hkswzghtmeeftjmnnsmx.supabase.co",
 "sb_publishable_5YfaypCKMP8y_H_u3_dBGw_MZ9CIPG9"
 )
@@ -12,6 +12,8 @@ if(pw==="donghae2026"){
 document.getElementById("login").style.display="none"
 document.getElementById("panel").style.display="block"
 
+loadList()
+
 }
 
 }
@@ -23,10 +25,51 @@ const content=document.getElementById("content").value
 const type=document.getElementById("type").value
 const due_date=document.getElementById("date").value
 
-await supabase
+await supabaseClient
 .from("notices")
 .insert([{title,content,type,due_date}])
 
 alert("추가 완료")
+
+loadList()
+
+}
+
+async function loadList(){
+
+const { data } = await supabaseClient
+.from("notices")
+.select("*")
+.order("created_at",{ascending:false})
+
+const container=document.getElementById("list")
+container.innerHTML=""
+
+data.forEach(n=>{
+
+const div=document.createElement("div")
+div.className="card"
+
+div.innerHTML=`
+<b>${n.title}</b>
+<span class="adminBtn" onclick="deleteNotice(${n.id})">삭제</span>
+`
+
+container.appendChild(div)
+
+})
+
+}
+
+async function deleteNotice(id){
+
+if(!confirm("삭제?")) return
+
+await supabaseClient
+.from("notices")
+.delete()
+.eq("id",id)
+
+loadList()
 
 }
